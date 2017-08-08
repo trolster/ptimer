9148b4db6e67ba0091307d68e8aa3e0309b9a62d
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-08-08 20:03:06 +0200

    Add working timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello, World!");
+#!/usr/bin/env node
+
+// npm modules
+import cli from "commander";
+import ProgressBar from "progress";
+// our modules
+import pkg from "../package.json";
+
+cli
+  .version(pkg.version)
+  .option("-p, --project [name]", "Log the project")
+  .parse(process.argv);
+
+const projectName = cli.project || "random";
+const pomodoroInSeconds = 60;
+
+const bar = new ProgressBar(":bar", { total: pomodoroInSeconds });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    console.log("\ncomplete\n");
+    clearInterval(timer);
+  }
+}, 1000);
+
+// output all uncaught exceptions
+process.on("uncaughtException", err => {
+  console.error("uncaught exception:", err);
+});
+// output all uncaught promise rejections
+process.on("unhandledRejection", err => {
+  console.error("unhandled rejection:", err);
+});
